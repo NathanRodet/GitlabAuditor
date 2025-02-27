fec69ddbf30ba896c79a5b5acff6272be2c9b594
Nathan Rodet <rodetnathan.contact@gmail.com>
2025-02-28 00:30:38 +0100

    feat: started to implement Gitlab API group fetching

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use gitlab_auditor::cli::Args;
 use gitlab_auditor::cli::return_args;
+use gitlab_auditor::scans::group::fetch_groups;
 
 #[tokio::main]
 async fn main() {
@@ -9,4 +10,7 @@ async fn main() {
     println!("GitLab Token: {}", args.gitlab_token);
     println!("Instance URL: {}", args.instance_url);
     println!("Scan type: {:?}", args.scan_type);
+
+    let groups = fetch_groups(&args.gitlab_token, &args.instance_url).await;
+    println!("Groups: {:?}", groups);
 }
---
+++ src/scans/shared.rs
@@ -0,0 +1,2 @@
+pub const PRIVATE_TOKEN_HEADER: &str = "PRIVATE-TOKEN";
+pub const MIN_ACCESS_LEVEL_GUEST: &str = "10";
